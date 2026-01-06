719e36cdd235a247eec55d6587494386827f3db6
Antoniooo <160254890+NFinite-Aspire@users.noreply.github.com>
2026-01-06 15:46:28 +0200

    Add files via upload

---
+++ Conversie de baze/Conversie de baze/Program.cs
@@ -0,0 +1,20 @@
+﻿//titlu
+Console.WriteLine("==Conversie de baze==");
+
+//baza initiala
+Console.Write("alege baza din care vrei sa convertesti (2, 8, 10, 16): ");
+int bazainitiala = int.Parse(Console.ReadLine());
+
+//baza finala
+Console.Write("alege baza in care vrei sa convertesti (2, 8, 10, 16): ");
+int bazafinala = int.Parse(Console.ReadLine());
+
+//numarul de convertit
+Console.Write("introdu numarul pe care vrei sa-l convertesti: ");
+string numar = Console.ReadLine();
+
+//rezultatul conversiei
+int value = Convert.ToInt32(numar, bazainitiala);
+string rezultat = Convert.ToString(value, bazafinala).ToUpper();
+Console.WriteLine("rezultatul este:");
+Console.WriteLine(rezultat);
